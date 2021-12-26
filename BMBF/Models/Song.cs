0131274c4266cf4b2454a18d79804648549d278c
Unicorns <unicorns@bmbf.dev>
2021-12-26 08:19:58 +0000

    Add song model

---
+++ BMBF/Models/Song.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace BMBF.Models
+{
+    /// <summary>
+    /// Represents a Beat Saber song
+    /// </summary>
+    public class Song
+    {
+        public string Hash { get; }
+        
+        public string SongName { get; }
+        
+        public string SongSubName { get; }
+        
+        public string SongAuthorName { get; }
+        
+        public string LevelAuthorName { get; }
+        
+        // The below properties will not be sent to the frontend
+        [JsonIgnore] 
+        public string Path { get; }
+        
+        [JsonIgnore]
+        public string CoverImageFileName { get; }
+        
+        [JsonConstructor]
+        internal Song(string hash, string songName, string songSubName, string songAuthorName, string levelAuthorName, string path, string coverImageFileName)
+        {
+            Hash = hash;
+            SongName = songName;
+            SongSubName = songSubName;
+            SongAuthorName = songAuthorName;
+            LevelAuthorName = levelAuthorName;
+            Path = path;
+            CoverImageFileName = coverImageFileName;
+        }
+    }
+}
