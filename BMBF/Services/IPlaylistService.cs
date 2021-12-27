6f19da0aef74df4eee746f2ed8ed9921d314e37d
Unicorns <unicorns@bmbf.dev>
2021-12-27 07:41:02 +0000

    Add playlist and song services

---
+++ BMBF/Services/IPlaylistService.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BMBF.Models;
+
+namespace BMBF.Services
+{
+    /// <summary>
+    /// Manages the BMBF playlist cache (in-memory only, we do not save it to disk since loading playlists is not particularly expensive)
+    /// </summary>
+    public interface IPlaylistService
+    {
+        /// <summary>
+        /// Gets or loads the playlists from the playlist folder
+        /// </summary>
+        /// <returns>A dictionary of the loaded playlists. Key is playlist path</returns>
+        ValueTask<IReadOnlyDictionary<string, Playlist>> GetPlaylistsAsync();
+        
+        /// <summary>
+        /// Updates the current playlist cache
+        /// If it has not yet been loaded, this will just load the cache
+        /// </summary>
+        Task UpdatePlaylistCacheAsync();
+
+        /// <summary>
+        /// Deletes the given playlist
+        /// </summary>
+        /// <param name="playlist">The playlist to delete</param>
+        /// <returns>Whether or not the playlist was contained within the cache</returns>
+        Task<bool> DeletePlaylistAsync(Playlist playlist);
+
+        /// <summary>
+        /// Invoked whenever a playlist is added
+        /// </summary>
+        event EventHandler<Playlist> PlaylistAdded;
+        
+        /// <summary>
+        /// Invoked whenever a playlist is deleted
+        /// </summary>
+        event EventHandler<Playlist> PlaylistDeleted;
+    }
+}
---
+++ BMBF/Services/ISongService.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BMBF.Models;
+
+namespace BMBF.Services
+{
+    /// <summary>
+    /// Manages the BMBF song cache
+    /// </summary>
+    public interface ISongService
+    {
+        /// <summary>
+        /// Gets a dictionary of all loaded songs.
+        /// Keys are the song's SHA-1 hash.
+        /// </summary>
+        /// <returns>An enumerator over the loaded songs</returns>
+        ValueTask<IReadOnlyDictionary<string, Song>> GetSongsAsync();
+
+        /// <summary>
+        /// Updates the current song cache
+        /// </summary>
+        Task UpdateSongCacheAsync();
+
+        /// <summary>
+        /// Deletes the song(s) with the given hash, if a song with the hash is loaded.
+        /// </summary>
+        /// <param name="hash">The SHA-1 hash of the song to delete, upper case</param>
+        /// <returns>Whether or not any songs with the given hash existed and were deleted</returns>
+        Task<bool> DeleteSongAsync(string hash);
+
+        /// <summary>
+        /// Invoked whenever a new song is loaded
+        /// </summary>
+        event EventHandler<Song>? SongAdded; 
+        
+        /// <summary>
+        /// Invoked whenever a song is removed
+        /// </summary>
+        event EventHandler<Song>? SongRemoved;
+    }
+}
