0b9032dee46fe516b0872f46f866e50a422c8e19
Unicorns <unicorns@bmbf.dev>
2021-12-31 08:42:22 +0000

    Add endpoint for fetching install info

---
+++ BMBF/Controllers/BeatSaberController.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+
+using System.Threading.Tasks;
+using BMBF.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMBF.Controllers
+{
+    [Route("[controller]")]
+    public class BeatSaberController : Controller
+    {
+        private readonly IBeatSaberService _beatSaberService;
+
+        public BeatSaberController(IBeatSaberService beatSaberService)
+        {
+            _beatSaberService = beatSaberService;
+        }
+
+        [Route("install")]
+        public async Task<IActionResult> GetInstallInfo()
+        {
+            var installInfo = await _beatSaberService.GetInstallationInfoAsync();
+            if (installInfo is null)
+            {
+                return NotFound();
+            }
+            return new OkObjectResult(installInfo);
+        }
+    }
+}
