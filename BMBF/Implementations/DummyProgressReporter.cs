0c746b07ab35f4ee0b3180a66c85dc3428e9ae47
Unicorns <unicorns@bmbf.dev>
2022-01-01 17:28:34 +0000

    Add dummy progress reporter for downgrading This is temporary - we'll likely want a progress bar on the frontend in the future

---
+++ BMBF/Implementations/DummyProgressReporter.cs
@@ -0,0 +1,9 @@
+﻿using Octodiff.Diagnostics;
+
+namespace BMBF.Implementations
+{
+    public class DummyProgressReporter : IProgressReporter
+    {
+        public void ReportProgress(string operation, long currentPosition, long total) { }
+    }
+}
