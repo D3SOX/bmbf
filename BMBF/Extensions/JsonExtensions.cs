3a41c1dc40c562e1bf2d68cb62791c7cc66318ce
Unicorns <unicorns@bmbf.dev>
2022-01-02 17:11:30 +0000

    Refactor JsonExtensions.cs

--- BMBF/Extensions/JsonExtensions.cs
+++ BMBF/Extensions/JsonExtensions.cs
@@ -7,23 +7,31 @@ namespace BMBF.Extensions
 {
     public static class JsonExtensions
     {
-        private static JsonSerializer JsonSerializer = new JsonSerializer
+        private static readonly JsonSerializer CamelCaseJsonSerializer = new JsonSerializer
         {
             ContractResolver = new CamelCasePropertyNamesContractResolver()
         };
+
+        private static readonly JsonSerializer DefaultJsonSerializer = new JsonSerializer();
         
-        public static T ReadAsCamelCaseJson<T>(this Stream stream)
+        public static T ReadAsJson<T>(this Stream stream, JsonSerializer? jsonSerializer = null)
         {
             using var reader = new StreamReader(stream);
             using var jsonReader = new JsonTextReader(reader);
-            return JsonSerializer.Deserialize<T>(jsonReader) ?? throw new NullReferenceException("Deserialized result was null");
+            return (jsonSerializer ?? DefaultJsonSerializer).Deserialize<T>(jsonReader) ?? throw new NullReferenceException("Deserialized result was null");
         }
 
-        public static void WriteAsCamelCaseJson(this object o, Stream stream)
+        public static T ReadAsCamelCaseJson<T>(this Stream stream) => 
+            ReadAsJson<T>(stream, CamelCaseJsonSerializer);
+
+        public static void WriteAsJson(this object o, Stream stream, JsonSerializer? jsonSerializer = null)
         {
             using var writer = new StreamWriter(stream);
             using var jsonWriter = new JsonTextWriter(writer);
-            JsonSerializer.Serialize(jsonWriter, o);
+            (jsonSerializer ?? DefaultJsonSerializer).Serialize(jsonWriter, o);
         }
+
+        public static void WriteAsCamelCaseJson(this object o, Stream stream) =>
+            WriteAsJson(o, stream, CamelCaseJsonSerializer);
     }
 }
