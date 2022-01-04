25167c5cde3f4606f3455e6bd345f52beb5cf22c
Unicorns <unicorns@bmbf.dev>
2022-01-04 08:15:14 +0000

    Add QMOD implementation

---
+++ BMBF.QMod/QMod.cs
@@ -0,0 +1,267 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BMBF.ModManagement;
+using QuestPatcher.QMod;
+
+namespace BMBF.QMod
+{
+    public class QMod : IMod
+    {
+        public string Id => Mod.Id;
+        public string Name => Mod.Name;
+        public string Author => Mod.Author;
+        public string? Porter => Mod.Porter;
+        public string? Description => Mod.Description;
+        public string PackageVersion => Mod.PackageVersion;
+        public string Version => Mod.Version.ToString();
+        public bool Installed { get; private set; }
+        
+        internal QuestPatcher.QMod.QMod Mod { get; }
+
+        private IFileSystem FileSystem => _provider.FileSystem;
+        private HttpClient HttpClient => _provider.HttpClient;
+        
+        private readonly QModProvider _provider;
+        
+        public QMod(QuestPatcher.QMod.QMod mod, QModProvider provider)
+        {
+            Mod = mod;
+            _provider = provider;
+        }
+        
+        
+        public async Task InstallAsync()
+        {
+            await _provider.InstallLock.WaitAsync();
+            try
+            {
+                await InstallAsyncInternal(new HashSet<string>());
+            }
+            finally
+            {
+                _provider.InstallLock.Release();
+            }
+        }
+        
+        public async Task UninstallAsync()
+        {
+            await _provider.InstallLock.WaitAsync();
+            try
+            {
+                await UninstallAsyncInternal();
+            }
+            finally
+            {
+                _provider.InstallLock.Release();
+            }
+        }
+        
+        public Stream? OpenCoverImage()
+        {
+            return Mod.CoverImagePath == null ? null : Mod.OpenCoverImage();
+        }
+
+        internal void UpdateStatusInternal()
+        {
+            // Check that all the mod files, library files, and file copies are installed before setting the mod as installed
+            
+            foreach (string m in Mod.ModFileNames)
+            {
+                if(!FileSystem.File.Exists(Path.Combine(_provider.ModsPath, Path.GetFileName(m))))
+                {
+                    Installed = false;
+                    return;
+                }
+            }
+            
+            foreach (string lib in Mod.LibraryFileNames)
+            {
+                if(!FileSystem.File.Exists(Path.Combine(_provider.LibsPath, Path.GetFileName(lib))))
+                {
+                    Installed = false;
+                    return;
+                }
+            }
+            
+            foreach (var fileCopy in Mod.FileCopies)
+            {
+                if(!FileSystem.File.Exists(fileCopy.Destination))
+                {
+                    Installed = false;
+                    return;
+                }
+            }
+
+            Installed = true;
+        }
+
+        internal async Task InstallAsyncInternal(HashSet<string> installPath)
+        {
+            installPath.Add(Id);
+            try
+            {
+                // Install dependencies first
+                foreach (var dependency in Mod.Dependencies)
+                {
+                    await PrepareDependency(dependency, installPath);
+                }
+
+                // Make sure that destination paths exist first
+                FileSystem.Directory.CreateDirectory(_provider.ModsPath);
+                FileSystem.Directory.CreateDirectory(_provider.LibsPath);
+
+                // Copy over all of the mod files, lib files and file copies in order to actually install the mod
+                
+                foreach (var m in Mod.ModFileNames)
+                {
+                    string destPath = Path.Combine(_provider.ModsPath, Path.GetFileName(m));
+                    await using var modStream = Mod.OpenModFile(m);
+                    await ExtractAsync(modStream, destPath).ConfigureAwait(false);
+                }
+                
+                foreach (var lib in Mod.LibraryFileNames)
+                {
+                    string destPath = Path.Combine(_provider.LibsPath, Path.GetFileName(lib));
+                    await using var libStream = Mod.OpenLibraryFile(lib);
+                    await ExtractAsync(libStream, destPath).ConfigureAwait(false);
+                }
+                
+                foreach (var fc in Mod.FileCopies)
+                {
+                    await using var copyStream = Mod.OpenFileCopy(fc);
+
+                    // Create the directory if it does not already exist
+                    var directoryName = Path.GetDirectoryName(fc.Destination);
+                    if (directoryName != null)
+                    {
+                        FileSystem.Directory.CreateDirectory(directoryName);
+                    }
+                    
+                    await ExtractAsync(copyStream, fc.Destination).ConfigureAwait(false);
+                }
+
+                Installed = true;
+                _provider.InvokeModStatusChanged(this);
+            }
+            finally
+            {
+                installPath.Remove(Id);
+            }
+        }
+
+        internal async Task<List<QMod>> UninstallAsyncInternal()
+        {
+            // Uninstall ourself
+            
+            foreach (string m in Mod.ModFileNames)
+            {
+                string destPath = Path.Combine(_provider.ModsPath, Path.GetFileName(m));
+                if(FileSystem.File.Exists(destPath)) FileSystem.File.Delete(destPath);
+            }
+            
+            foreach (string lib in Mod.LibraryFileNames)
+            {
+                // Skip library files still in use by other libraries
+                if (_provider.Mods.Values.Any(mod => mod.Id != this.Id && mod.Installed && mod.Mod.LibraryFileNames.Contains(lib))) continue;
+
+                string destPath = Path.Combine(_provider.LibsPath, Path.GetFileName(lib));
+                if(FileSystem.File.Exists(destPath)) FileSystem.File.Delete(destPath);
+            }
+            
+            foreach (var fileCopy in Mod.FileCopies)
+            {
+                if(FileSystem.File.Exists(fileCopy.Destination)) FileSystem.File.Delete(fileCopy.Destination);
+            }
+
+            // Set ourself to uninstalled to avoid being included with potentially recursive dependency uninstalls
+            Installed = false;
+
+            // Uninstall mods depending on this mod (and collect in list)
+            var uninstalledDependants = new List<QMod>();
+            foreach (QMod mod in _provider.Mods.Values.Where(m => m.Installed && m.Mod.Dependencies.Any(d => d.Id == Id)))
+            {
+                uninstalledDependants.Add(mod);
+                await mod.UninstallAsyncInternal().ConfigureAwait(false);
+            }
+            
+            // Uninstall libraries that no installed mods depend on
+            foreach(QMod mod in _provider.Mods.Values.Where(m =>
+                        m.Installed &&
+                        m.Mod.IsLibrary &&
+                        !_provider.Mods.Values.Any(dependingMod => dependingMod.Installed && dependingMod.Mod.Dependencies.Any(d => d.Id == m.Id))))
+            {
+                await mod.UninstallAsyncInternal().ConfigureAwait(false);
+            }
+            
+            _provider.InvokeModStatusChanged(this); // Now we actually forward the uninstall to the frontend
+            return uninstalledDependants;
+        }
+        
+        private async Task PrepareDependency(Dependency dependency, HashSet<string> installPath)
+        {
+            // Cyclical dependency!
+            if (installPath.Contains(dependency.Id))
+            {
+                return;
+            }
+            
+            var existing = _provider.Mods.Values.FirstOrDefault(m => m.Mod.Id == dependency.Id);
+            if (existing != null)
+            {
+                if (dependency.VersionRange.IsSatisfied(existing.Mod.Version))
+                {
+                    // Dependency is already loaded and within the version range.
+                    if (!existing.Installed)
+                    {
+                        await existing.InstallAsyncInternal(installPath).ConfigureAwait(false); // Install it if it is not already installed
+                    }
+                    return;
+                }
+                // Otherwise we will need to upgrade the dependency
+                if(dependency.DownloadIfMissing == null)
+                {
+                    throw new InstallationException(
+                        $"Dependency {dependency.Id} is installed, but with an incorrect version ({existing.Mod.Version}) and does not specify a download link if missing, so the version couldn't be upgraded");
+                }
+            }
+            else if(dependency.DownloadIfMissing == null)
+            {
+                throw new InstallationException($"Dependency {dependency.Id} is not installed, and does not specify a download link if missing");
+            }
+            
+            try
+            {
+                using var resp = await HttpClient.GetAsync(dependency.DownloadIfMissing).ConfigureAwait(false);
+                await using var content = await resp.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+                var installedDep = await _provider.ImportModAsyncInternal(content, installPath);
+                // Quick sanity check to avoid people putting invalid download links and not noticing
+                if (!dependency.VersionRange.IsSatisfied(installedDep.Version))
+                {
+                    throw new InstallationException(
+                        $"Dependency downloaded from {dependency.DownloadIfMissing} had version {installedDep.Version}, which did not satisfy {dependency.VersionRange}");
+                }
+
+                // Actually install the dependency, which may involve installing more dependencies 
+                await installedDep.InstallAsyncInternal(installPath).ConfigureAwait(false);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new InstallationException($"Failed to download dependency {dependency.Id} from {dependency.DownloadIfMissing}", ex);
+            }
+        }
+        
+        private async Task ExtractAsync(Stream stream, string destination)
+        {
+            // If files exist, we always overwrite them first
+            if (FileSystem.File.Exists(destination)) FileSystem.File.Delete(destination);
+            await using var destStream = FileSystem.File.OpenWrite(destination);
+            
+            await stream.CopyToAsync(destStream).ConfigureAwait(false);
+        }
+    }
+}
---
+++ BMBF.QMod/QModProvider.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using BMBF.ModManagement;
+using QuestPatcher.QMod;
+
+namespace BMBF.QMod
+{
+    public class QModProvider : IModProvider
+    {
+        public const string ModExtension = ".qmod";
+
+        internal Dictionary<string, QMod> Mods { get; } = new Dictionary<string, QMod>();
+
+        internal string PackageId { get; }
+        internal string ModsPath { get; }
+        internal string LibsPath { get; }
+        
+        internal HttpClient HttpClient { get; }
+        internal IFileSystem FileSystem { get; }
+        internal SemaphoreSlim InstallLock { get; } = new SemaphoreSlim(1);
+
+        public event EventHandler<ModLoadedEventArgs>? ModLoaded;
+
+        public event EventHandler<IMod>? ModUnloaded;
+        
+        public event EventHandler<IMod>? ModStatusChanged;
+
+        public bool CanAttemptImport(string fileName)
+        {
+            return Path.GetExtension(fileName).ToLower() == ModExtension;
+        }
+
+        public QModProvider(string packageId, string modsPath, string libsPath, HttpClient httpClient, IFileSystem fileSystem)
+        {
+            PackageId = packageId;
+            ModsPath = modsPath;
+            LibsPath = libsPath;
+            HttpClient = httpClient;
+            FileSystem = fileSystem;
+        }
+
+        public async ValueTask<IMod?> TryImportModAsync(Stream stream, string fileName)
+        {
+            if (Path.GetExtension(fileName).ToLower() != ModExtension)
+            {
+                // Can't process mods which aren't qmod files
+                return null;
+            }
+
+            await InstallLock.WaitAsync();
+            try
+            {
+                return await ImportModAsyncInternal(stream, new HashSet<string>());
+            }
+            finally
+            {
+                InstallLock.Release();
+            }
+        }
+        
+        public async Task<bool> UnloadModAsync(IMod genericMod)
+        {
+            await InstallLock.WaitAsync();
+            try
+            {
+                if (!(genericMod is QMod mod)) return false;
+                
+                if (mod.Installed)
+                {
+                    await mod.UninstallAsyncInternal();
+                }
+                return UnloadModInternal(mod);
+            }
+            finally
+            {
+                InstallLock.Release();
+            }
+        }
+
+        internal void InvokeModStatusChanged(QMod mod)
+        {
+            ModStatusChanged?.Invoke(this, mod);
+        }
+
+        private bool UnloadModInternal(QMod mod)
+        {
+            if (Mods.Remove(mod.Id, out var removed))
+            {
+                ModUnloaded?.Invoke(this, removed);
+                return true;
+            }
+            return false;
+        }
+
+        internal async Task<QMod> ImportModAsyncInternal(Stream stream, HashSet<string> installPath)
+        {
+            ZipArchive modArchive;
+            try
+            {
+                modArchive = new ZipArchive(stream, ZipArchiveMode.Read, true);
+            }
+            catch (InvalidDataException)
+            {
+                throw new InstallationException("Mod was not a valid ZIP archive");
+            }
+            
+            QuestPatcher.QMod.QMod qMod;
+            try
+            {
+                qMod = await QuestPatcher.QMod.QMod.ParseAsync(modArchive, false);
+            }
+            catch (InvalidModException ex)
+            {
+                throw new InstallationException(ex.Message);
+            }
+            catch (InvalidDataException)
+            {
+                throw new InstallationException("Mod was not a valid ZIP archive");
+            }
+            
+            // Make sure that the mod is for the correct app!
+            if (qMod.PackageId != PackageId)
+            {
+                throw new InstallationException($"Mod was for package id {qMod.PackageId}, not {PackageId}");
+            }
+
+            var mod = new QMod(qMod, this);
+            
+            mod.UpdateStatusInternal(); // Check whether or not the mod is installed
+            
+            stream.Position = 0;
+
+            // If an existing mod exists with this same ID, we will need to uninstall it
+            // This may uninstall several dependant mods
+            List<QMod> uninstalledDependants = new List<QMod>();
+            if (Mods.TryGetValue(mod.Id, out var existing))
+            {
+                if (existing.Installed)
+                {
+                    uninstalledDependants = await existing.UninstallAsyncInternal();
+                }
+
+                UnloadModInternal(existing);
+            }
+            
+            Mods.Add(mod.Id, mod);
+            ModLoaded?.Invoke(mod.Id, new ModLoadedEventArgs(mod, stream, $"{mod.Id}_v{mod.Version}.qmod"));
+
+            if (uninstalledDependants.Count > 0)
+            {
+                // Install the mod now, so that we can reinstall dependencies
+                await mod.InstallAsyncInternal(installPath); 
+                
+                // Now reinstall the dependant mods
+                foreach (QMod uninstalledDependant in uninstalledDependants)
+                {
+                    // Cannot reinstall dependant mod - version range is not satisfied
+                    if (!uninstalledDependant.Mod.Dependencies.Single(d => d.Id == mod.Id).VersionRange
+                            .IsSatisfied(mod.Version))
+                    {
+                        continue;
+                    }
+                    
+                    await uninstalledDependant.InstallAsyncInternal(installPath);
+                }
+            }
+
+            return mod;
+        }
+    }
+}
