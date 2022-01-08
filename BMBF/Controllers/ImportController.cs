3f73c85db97edfceb964a8d86cea6d82f5fbf966
Unicorns <unicorns@bmbf.dev>
2022-01-08 15:06:33 +0000

    Add POST /api/import endpoint

---
+++ BMBF/Controllers/ImportController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using BMBF.Models;
+using BMBF.Services;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace BMBF.Controllers
+{
+    [Route("[controller]")]
+    public class ImportController : Controller
+    {
+        private readonly IFileImporter _fileImporter;
+
+        public ImportController(IFileImporter fileImporter)
+        {
+            _fileImporter = fileImporter;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportFile()
+        {
+            if (!Request.Headers.TryGetValue("filename", out var fileName))
+            {
+                return BadRequest("Cannot import file without filename");
+            }
+            
+            var result = await _fileImporter.TryImportAsync(Request.Body, fileName);
+            if (result.Type == FileImportResultType.Failed)
+            {
+                Log.Error($"Failed to import file {fileName}: {result.Error}");
+            }
+            return Ok(result);
+        }
+    }
+}
