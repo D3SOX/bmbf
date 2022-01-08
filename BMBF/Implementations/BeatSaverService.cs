6d5080b02b59cf135de518c9d83270535045df83
Unicorns <unicorns@bmbf.dev>
2022-01-08 14:50:22 +0000

    Add service for song downloads

---
+++ BMBF/Implementations/BeatSaverService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BMBF.Services;
+using BMBF.Util;
+using Newtonsoft.Json.Linq;
+
+namespace BMBF.Implementations
+{
+    public class BeatSaverService : IBeatSaverService
+    {
+        private readonly HttpClient _httpClient;
+        
+        public BeatSaverService(BMBFSettings settings)
+        {
+            _httpClient = HttpClientUtil.CreateBMBFHttpClient();
+            _httpClient.BaseAddress = settings.BeatSaverBaseUri;
+        }
+
+        public async Task<Stream?> DownloadSongByHash(string hash)
+        {
+            return await FindAndDownload($"maps/hash/{hash}");
+        }
+
+        public async Task<Stream?> DownloadSongByKey(string key)
+        {
+            return await FindAndDownload($"maps/id/{key}");
+        }
+
+        private async Task<Stream?> FindAndDownload(string mapInfoUri)
+        {
+            using var resp = await _httpClient.GetAsync(mapInfoUri, HttpCompletionOption.ResponseHeadersRead);
+            if (resp.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            
+            string respString = await resp.Content.ReadAsStringAsync();
+
+            var versions = JToken.Parse(respString).Value<JArray>("versions");
+            if (versions == null)
+            {
+                throw new FormatException("Map had no versions property");
+            }
+            
+            DateTime latest = DateTime.MinValue;
+            string? latestDownloadUri = null;
+            foreach (var versionObj in versions)
+            {
+                var val = versionObj.Value<DateTime>("createdAt");
+                if (latest < val)
+                {
+                    latest = val;
+                    latestDownloadUri = versionObj.Value<string>("downloadURL");
+                }
+            }
+            
+            var mapResp = await _httpClient.GetAsync(latestDownloadUri, HttpCompletionOption.ResponseHeadersRead);
+            mapResp.EnsureSuccessStatusCode();
+            return await mapResp.Content.ReadAsStreamAsync();
+        }
+    }
+}
---
+++ BMBF/Services/IBeatSaverService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace BMBF.Services
+{
+    /// <summary>
+    /// Represents the BeatSaver song repository.
+    /// </summary>
+    public interface IBeatSaverService
+    {
+        /// <summary>
+        /// Downloads a song, given its song hash.
+        /// Uses the latest revision of the song.
+        /// </summary>
+        /// <param name="hash">Hash of the song to upload</param>
+        /// <returns>Stream of the song download, or null if no song with the given hash existed on BeatSaver</returns>
+        Task<Stream?> DownloadSongByHash(string hash);
+
+        /// <summary>
+        /// Downloads a song, given its beatsaver key
+        /// Uses the latest revision of the song
+        /// </summary>
+        /// <param name="key">Beatsaver key of the song to download</param>
+        /// <returns>Stream of the song download, or null if no song with the given key existed on BeatSaver</returns>
+        Task<Stream?> DownloadSongByKey(string key);
+    }
+}
