07973547fd13b32c30d27b8905efeefc91fbc3be
Unicorns <unicorns@bmbf.dev>
2022-01-21 17:55:53 +0000

    Replace restore script with MSBuild target Easier to use, prints warning message if nuget isn't on PATH

--- restore.ps1
+++
@@ -1,7 +0,0 @@
-Write-Output "Restoring BMBF:"
-
-# packages.config contains the aspnetcore runtime packages, as dotnet restore is refusing to restore these automatically
-nuget restore packages.config -PackagesDirectory $HOME/.nuget/packages
-
-# Restore the actual BMBF project
-nuget restore BMBF.sln
