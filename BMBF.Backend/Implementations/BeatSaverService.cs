bfebe3f5e863807e048e8b19e755dafdb7c030f7
Unicorns <unicorns@bmbf.dev>
2022-01-27 17:44:47 +0000

    Fix BeatSaverService finds song with incorrect hash

--- BMBF.Backend/Implementations/BeatSaverService.cs
+++ BMBF.Backend/Implementations/BeatSaverService.cs
@@ -22,30 +22,37 @@ public class BeatSaverService : IBeatSaverService
 
     public async Task<Stream?> DownloadSongByHash(string hash)
     {
-        return await FindAndDownload($"maps/hash/{hash}");
+        return await FindAndDownload($"maps/hash/{hash}", hash);
     }
 
     public async Task<Stream?> DownloadSongByKey(string key)
     {
-        return await FindAndDownload($"maps/id/{key}");
+        return await FindAndDownload($"maps/id/{key}", null);
     }
 
-    private async Task<Stream?> FindAndDownload(string mapInfoUri)
+    private async Task<Stream?> FindAndDownload(string mapInfoUri, string? preferHash)
     {
         using var resp = await _httpClient.GetAsync(mapInfoUri, HttpCompletionOption.ResponseHeadersRead);
         if (resp.StatusCode == HttpStatusCode.NotFound)
         {
             return null;
         }
-            
-        string respString = await resp.Content.ReadAsStringAsync();
 
-        var versions = JsonDocument.Parse(respString).RootElement.GetProperty("versions");
+        await using var respStream = await resp.Content.ReadAsStreamAsync();
+        var versions = (await JsonDocument.ParseAsync(respStream)).RootElement.GetProperty("versions");
 
         DateTime latest = DateTime.MinValue;
         string? latestDownloadUri = null;
         foreach (var versionObj in versions.EnumerateArray())
         {
+            var hash = versionObj.GetProperty("hash").GetString()?.ToUpper();
+            if(hash == null) continue;
+
+            if (preferHash != null && hash != preferHash)
+            {
+                continue;
+            }
+            
             var val = versionObj.GetProperty("createdAt").Deserialize<DateTime>();
             if (latest < val)
             {
@@ -53,7 +60,13 @@ public class BeatSaverService : IBeatSaverService
                 latestDownloadUri = versionObj.GetProperty("downloadURL").GetString();
             }
         }
-            
+
+        // No version found (with correct hash)
+        if (latestDownloadUri == null)
+        {
+            return null;
+        }
+
         var mapResp = await _httpClient.GetAsync(latestDownloadUri, HttpCompletionOption.ResponseHeadersRead);
         mapResp.EnsureSuccessStatusCode();
         return await mapResp.Content.ReadAsStreamAsync();
