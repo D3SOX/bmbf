b20cb56d7a07bd3a559d936b2a256434701b7cd6
Unicorns <unicorns@bmbf.dev>
2022-01-28 07:52:18 +0000

    Use constructor instead of Create methods

--- BMBF.Backend.Tests/BeatSaverServiceTests.cs
+++ BMBF.Backend.Tests/BeatSaverServiceTests.cs
@@ -13,38 +13,35 @@ using Xunit;
 
 namespace BMBF.Backend.Tests;
 
-public class BeatSaverServiceTests
+public class BeatSaverServiceTests : IDisposable
 {
     private Uri BaseAddress => new("https://example.com");
-    
-    /// <summary>
-    /// Creates a new instance of <see cref="BeatSaverService"/> with a mock <see cref="HttpClient"/>
-    /// </summary>
-    /// <returns>The created <see cref="BeatSaverService"/> and <see cref="HttpMessageHandler"/> mock for configuration</returns>
-    private (BeatSaverService, Mock<HttpMessageHandler>) Create()
-    {
-        var handlerMock = new Mock<HttpMessageHandler>();
 
-        var httpClient = new HttpClient(handlerMock.Object)
+    
+    public BeatSaverServiceTests()
+    {
+        _httpClient = new HttpClient(_handlerMock.Object)
         {
             BaseAddress = BaseAddress
         };
-        
-        return (new BeatSaverService(httpClient), handlerMock);
+        _beatSaverService = new BeatSaverService(_httpClient);
     }
 
+    private readonly Mock<HttpClientHandler> _handlerMock = new();
+    private readonly HttpClient _httpClient;
+    private readonly BeatSaverService _beatSaverService;
+
     /// <summary>
-    /// Sets up the given handler to return the <see cref="content"/> when the sub-url <see cref="requestUrl"/>.
-    /// <see cref="BaseAddress"/> will be automatically prepended to <see cref="requestUrl"/>
+    /// Sets up the client to return the <see cref="content"/> when the sub-url <see cref="requestUrl"/>.
     /// </summary>
-    /// <param name="handlerMock">Mock to register to content</param>
     /// <param name="requestUrl">URL for the content</param>
     /// <param name="content">Content to return</param>
-    private void SetupContent(Mock<HttpMessageHandler> handlerMock, string requestUrl, HttpContent content)
+    private void SetupGet(string requestUrl, HttpContent content)
     {
-        handlerMock.Protected()
+        _handlerMock.Protected()
             .Setup<Task<HttpResponseMessage>>("SendAsync",
-                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri == new Uri(BaseAddress, requestUrl)),
+                ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get && 
+                                                                                m.RequestUri == new Uri(BaseAddress, requestUrl)),
                 ItExpr.IsAny<CancellationToken>())
             .Returns(() => Task.FromResult(new HttpResponseMessage
             {
@@ -56,58 +53,58 @@ public class BeatSaverServiceTests
     /// <summary>
     /// Verifies that the given URL on the <see cref="HttpMessageHandler"/> mock was requested the given number of times.
     /// </summary>
-    /// <param name="handlerMock">Mock to verify</param>
     /// <param name="requestUrl">URL, <see cref="BaseAddress"/> will be automatically prepended to this</param>
     /// <param name="times">Number of times <paramref name="requestUrl"/> should have been requested</param>
-    private void VerifyRequested(Mock<HttpMessageHandler> handlerMock, string requestUrl, Times times)
+    private void VerifyGet(string requestUrl, Times times)
     {
-        handlerMock.Protected()
+        _handlerMock.Protected()
             .Verify<Task<HttpResponseMessage>>("SendAsync", times,
-                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri == new Uri(BaseAddress, requestUrl)),
+                ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get && m.RequestUri == new Uri(BaseAddress, requestUrl)),
                 ItExpr.IsAny<CancellationToken>());
     }
 
     /// <summary>
     /// Sets up the given map URL to return the given map versions
     /// </summary>
-    /// <param name="mapUrl"></param>
-    /// <param name="handlerMock"></param>
-    /// <param name="mapVersions"></param>
-    private void SetupVersions(Mock<HttpMessageHandler> handlerMock, string mapUrl, params MapVersion[] mapVersions)
+    /// <param name="mapUrl">URL of the map to setup versions for</param>
+    /// <param name="mapVersions">Map versions to return</param>
+    private void SetupGetVersions(string mapUrl, params MapVersion[] mapVersions)
     {
-        SetupContent(handlerMock, mapUrl, new StringContent(JsonSerializer.Serialize(new Map(mapVersions.ToList()))));
+        SetupGet(mapUrl, new StringContent(JsonSerializer.Serialize(new Map(mapVersions.ToList()))));
     }
 
     [Fact]
     public async Task ShouldChooseVersionWithCorrectHash()
     {
-        var (service, handlerMock) = Create();
-        SetupVersions(handlerMock, "maps/hash/1",
+        SetupGetVersions("maps/hash/1",
                 // Even though this version is older, it should be used as it matches the correct hash
                 new MapVersion("1", new Uri(BaseAddress, "map.zip"), new DateTime(0)),
                 new MapVersion("2", new Uri(BaseAddress, "wrongMap.zip"), new DateTime(1)));
-        SetupContent(handlerMock, "map.zip", new StringContent("example"));
+        SetupGet("map.zip", new StringContent("example"));
 
-        var result = await service.DownloadSongByHash("1");
+        var result = await _beatSaverService.DownloadSongByHash("1");
         Assert.NotNull(result);
-        VerifyRequested(handlerMock, "map.zip", Times.Once());
+        VerifyGet("map.zip", Times.Once());
     }
 
     [Fact]
     public async Task ShouldChooseLatestVersion()
     {
-        var (service, handlerMock) = Create();
-
-        SetupVersions(handlerMock, "maps/id/1",
+        SetupGetVersions("maps/id/1",
             // The newer map should always be downloaded when downloading by key
             new MapVersion("", new Uri(BaseAddress, "wrongMap.zip"), new DateTime(0)),
             new MapVersion("", new Uri(BaseAddress, "map.zip"), new DateTime(1)));
         
-        SetupContent(handlerMock, "map.zip", new StringContent("example"));
+        SetupGet("map.zip", new StringContent("example"));
 
-        var result = await service.DownloadSongByKey("1");
+        var result = await _beatSaverService.DownloadSongByKey("1");
         
         Assert.NotNull(result);
-        VerifyRequested(handlerMock, "map.zip", Times.Once());
+        VerifyGet("map.zip", Times.Once());
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
     }
 }
--- BMBF.Backend.Tests/FileImporterTests.cs
+++ BMBF.Backend.Tests/FileImporterTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.IO.Abstractions;
@@ -33,43 +32,50 @@ public class FileImporterTests
         ImmutableList<BPSong>.Empty,
         null
     );
-    
-    /// <summary>
-    /// Creates a <see cref="FileImporter"/> with either stubbed mocks, or the given override mocks.
-    /// </summary>
-    /// <returns>The instantiated file importer</returns>
-    private FileImporter Create(ISongService? overrideSongService = null,
-        IPlaylistService? overridePlaylistService = null,
-        IBeatSaverService? overrideBeatSaverService = null,
-        BMBFSettings? overrideSettings = null,
-        IAssetService? overrideAssetService = null,
-        IFileSystem? overrideFileSystem = null,
-        List<IMod>? overrideMods = null)
+
+    public FileImporterTests()
     {
-        var assetMock = new Mock<IAssetService>();
-        assetMock.Setup(m => m.GetExtensions())
+        _modServiceMock.Setup(m => m.GetModsAsync()).ReturnsAsync(new Dictionary<string, (IMod mod, string path)>());
+        _songServiceMock.Setup(s => s.GetSongsAsync()).ReturnsAsync(new Dictionary<string, Song>());
+        _assetServiceMock.Setup(m => m.GetExtensions())
         .ReturnsAsync(() => new FileExtensions(
             new Dictionary<string, string> { { "qsaber", "/Sabers" } },
             new List<string> { "json", "bplist" },
             new List<string> { "json", "yml" }
         ));
+        _fileImporter = new FileImporter(
+            _songServiceMock.Object,
+            _playlistServiceMock.Object,
+            _beatSaverServiceMock.Object,
+            _settings,
+            _modServiceMock.Object,
+            _assetServiceMock.Object,
+            _fileSystem
+        );
+    }
 
-        var modServiceMock = new Mock<IModService>();
-        modServiceMock.Setup(m => m.GetModsAsync())
-            .ReturnsAsync(overrideMods?.ToDictionary(m => m.Id, m => (m, "")) ?? new Dictionary<string, (IMod mod, string path)>());
-            
+    private readonly FileImporter _fileImporter;
+    private readonly Mock<ISongService> _songServiceMock = new();
+    private readonly Mock<IBeatSaverService> _beatSaverServiceMock = new();
+    private readonly Mock<IAssetService> _assetServiceMock = new();
+    private readonly Mock<IModService> _modServiceMock = new();
+    private readonly Mock<IPlaylistService> _playlistServiceMock = new();
+    
+    private readonly IFileSystem _fileSystem = new MockFileSystem();
+    private readonly BMBFSettings _settings = new()
+    {
+        ConfigsPath = "/Configs"
+    };
 
-        return new(
-            overrideSongService ?? Mock.Of<ISongService>(),
-            overridePlaylistService ?? Mock.Of<IPlaylistService>(),
-            overrideBeatSaverService ?? Mock.Of<IBeatSaverService>(),
-            overrideSettings ?? new BMBFSettings
-            {
-                ConfigsPath = "/Configs"
-            },
-            modServiceMock.Object,
-            overrideAssetService ?? assetMock.Object,
-            overrideFileSystem ?? new MockFileSystem());
+    /// <summary>
+    /// Sets up <see cref="_modServiceMock"/> to return the given mods
+    /// </summary>
+    /// <param name="mods">Mods to mock as loaded</param>
+    private void SetupMods(params IMod[] mods)
+    {
+        var modsDictionary = mods.ToDictionary(m => m.Id, m => (m, ""));
+        _modServiceMock.Setup(m => m.GetModsAsync())
+            .ReturnsAsync(modsDictionary);
     }
 
     /// <summary>
@@ -117,13 +123,11 @@ public class FileImporterTests
             Type = FileImportResultType.Song
         };
         
-        var songServiceMock = new Mock<ISongService>();
-        songServiceMock.Setup(s => 
+        _songServiceMock.Setup(s => 
             s.ImportSongAsync(It.IsNotNull<ZipArchive>(), It.IsAny<string>()))
             .ReturnsAsync(expectedResult);
         
-        var fileImporter = Create(songServiceMock.Object);
-        var result = await fileImporter.ImportAsync(songStream, "myFile.zip");
+        var result = await _fileImporter.ImportAsync(songStream, "myFile.zip");
         
         // Make sure that the result returns matches that returned by the ISongService
         Assert.Equal(expectedResult, result);
@@ -135,8 +139,7 @@ public class FileImporterTests
         // Blank stream will not be a valid archive
         using var modStream = new MemoryStream();
     
-        var fileImporter = Create();
-        var result = await fileImporter.TryImportAsync(modStream, "myFile.zip");
+        var result = await _fileImporter.TryImportAsync(modStream, "myFile.zip");
         
         Assert.Equal(FileImportResultType.Failed, result.Type);
     }
@@ -144,13 +147,11 @@ public class FileImporterTests
     [Fact]
     public async Task ShouldErrorWhenCannotLoadExtensions()
     {
-        var assetServiceMock = new Mock<IAssetService>();
-        assetServiceMock.Setup(a => a.GetExtensions()).ThrowsAsync(new HttpRequestException());
+        _assetServiceMock.Setup(a => a.GetExtensions()).ThrowsAsync(new HttpRequestException());
         
-        var fileImporter = Create(overrideAssetService: assetServiceMock.Object);
         using var blankStream = new MemoryStream();
         // QSABER is arbitrary, this can be any file type that is NOT .ZIP
-        var result = await fileImporter.TryImportAsync(blankStream, "myFile.qsaber");
+        var result = await _fileImporter.TryImportAsync(blankStream, "myFile.qsaber");
 
         Assert.Equal(FileImportResultType.Failed, result.Type);
     }
@@ -158,11 +159,9 @@ public class FileImporterTests
     [Fact]
     public async Task ShouldNotImportUnknownConfig()
     {
-        var fileImporter = Create();
-
         using var blankStream = new MemoryStream();
         // YAML is used instead of JSON here, since we don't want to trigger playlist importing
-        var result = await fileImporter.TryImportAsync(blankStream, "myUnknownConfig.yml");
+        var result = await _fileImporter.TryImportAsync(blankStream, "myUnknownConfig.yml");
         
         Assert.Equal(FileImportResultType.Failed, result.Type);
     }
@@ -173,23 +172,13 @@ public class FileImporterTests
         var modMock = new Mock<IMod>();
         modMock.SetupGet(m => m.Id)
             .Returns("example-mod");
-        var mod = modMock.Object;
-        
         // Add an example mod which will allow a config with its ID to be imported
-        var modServiceMock = new Mock<IModService>();
-        modServiceMock.Setup(m => m.GetModsAsync())
-            .ReturnsAsync(new Dictionary<string, (IMod mod, string path)>
-            {
-                {mod.Id, (mod, "")}
-            });
-
-        var fileSystem = new MockFileSystem();
-        var fileImporter = Create(overrideMods: new List<IMod>{ modMock.Object }, overrideFileSystem: fileSystem);
+        SetupMods(modMock.Object);
         
         // Import a config with the mod ID as the filename, and read the content placed in the config path
         using var exampleContent = CreateExampleContentStream();
-        await fileImporter.TryImportAsync(exampleContent, $"{mod.Id}.json");
-        byte[] content = fileSystem.File.ReadAllBytes("/Configs/example-mod.json");
+        await _fileImporter.TryImportAsync(exampleContent, $"{modMock.Object.Id}.json");
+        byte[] content = _fileSystem.File.ReadAllBytes(Path.Combine(_settings.ConfigsPath, "example-mod.json"));
 
         Assert.Equal(ExampleFileContent, content);
     }
@@ -203,13 +192,11 @@ public class FileImporterTests
             .Returns("example-mod");
         modMock.SetupGet(m => m.CopyExtensions)
             .Returns(new Dictionary<string, string> { { "png", "/ExampleModImages" } });
+        SetupMods(modMock.Object);
         
-        var fileSystem = new MockFileSystem();
-        var fileImporter = Create(overrideMods: new List<IMod>{ modMock.Object }, overrideFileSystem: fileSystem);
-
         using var exampleContent = CreateExampleContentStream();
-        await fileImporter.ImportAsync(exampleContent, "test.png");
-        byte[] content = fileSystem.File.ReadAllBytes("/ExampleModImages/test.png");
+        await _fileImporter.ImportAsync(exampleContent, "test.png");
+        byte[] content = _fileSystem.File.ReadAllBytes("/ExampleModImages/test.png");
 
         // Copy extensions stated by mods should be used when importing files,
         // so the file should be copied to ExampleModImages
@@ -219,12 +206,9 @@ public class FileImporterTests
     [Fact]
     public async Task ShouldUtiliseCopyExtension()
     {
-        var fileSystem = new MockFileSystem();
-        var fileImporter = Create(overrideFileSystem: fileSystem);
-        
         using var exampleContent = CreateExampleContentStream();
-        await fileImporter.ImportAsync(exampleContent, "test.qsaber");
-        byte[] content = fileSystem.File.ReadAllBytes("Sabers/test.qsaber");
+        await _fileImporter.ImportAsync(exampleContent, "test.qsaber");
+        byte[] content = _fileSystem.File.ReadAllBytes("Sabers/test.qsaber");
         
         // The built-in copy extensions contains an entry for .qsaber that should copy it to the path above
         Assert.Equal(ExampleFileContent, content);
@@ -240,10 +224,10 @@ public class FileImporterTests
         modMock.SetupGet(m => m.CopyExtensions)
             .Returns(new Dictionary<string, string> { { "qsaber", "/AnotherQSaberMod/" } });
         
-        var fileImporter = Create(overrideMods: new List<IMod>{ modMock.Object });
+        SetupMods(modMock.Object);
 
         using var exampleContent = CreateExampleContentStream();
-        var result = await fileImporter.TryImportAsync(exampleContent, "test.qsaber");
+        var result = await _fileImporter.TryImportAsync(exampleContent, "test.qsaber");
         
         // Multiple copy extensions just triggers failure at the moment
         Assert.Equal(FileImportResultType.Failed, result.Type);
@@ -255,17 +239,15 @@ public class FileImporterTests
         var examplePlaylist = ExamplePlaylist;
         const string expectedPlaylistId = "Example_Playlist";
         
-        var playlistServiceMock = new Mock<IPlaylistService>();
-        playlistServiceMock
+        _playlistServiceMock
             .Setup(p => p.AddPlaylistAsync(It.IsAny<Playlist>()))
             .ReturnsAsync(expectedPlaylistId);
-        var fileImporter = Create(overridePlaylistService: playlistServiceMock.Object);
 
         using var playlistStream = CreatePlaylistStream(examplePlaylist);
-        var result = await fileImporter.TryImportAsync(playlistStream, "test.bplist");
+        var result = await _fileImporter.TryImportAsync(playlistStream, "test.bplist");
         
         // Make sure that the playlist was added to the playlist service
-        playlistServiceMock.Verify(p => p.AddPlaylistAsync(
+        _playlistServiceMock.Verify(p => p.AddPlaylistAsync(
             It.Is<Playlist>(v => v.PlaylistTitle == examplePlaylist.PlaylistTitle)),
             Times.Once()
         );
@@ -282,33 +264,27 @@ public class FileImporterTests
             new BPSong(songHash, null, null)
         );
 
-        var songServiceMock = new Mock<ISongService>();
         var song = Song.CreateBlank();
         song.Hash = songHash;
-        songServiceMock.Setup(s => 
+        _songServiceMock.Setup(s => 
                 s.ImportSongAsync(It.IsNotNull<ZipArchive>(), It.IsNotNull<string>()))
             .ReturnsAsync(new FileImportResult
             {
                 ImportedSong = song,
                 Type = FileImportResultType.Song
             });
-        songServiceMock.Setup(s => s.GetSongsAsync())
-            .ReturnsAsync(new ConcurrentDictionary<string, Song>());
-        
-        var beatSaverMock = new Mock<IBeatSaverService>();
-        beatSaverMock.Setup(b => b.DownloadSongByHash(songHash))
-            .Returns((string _) => Task.FromResult(CreateEmptyZipStream())!);
+
+        _beatSaverServiceMock.Setup(b => b.DownloadSongByHash(songHash))
+            .ReturnsAsync(CreateEmptyZipStream());
 
         using var playlistStream = CreatePlaylistStream(examplePlaylist);
-        var fileImporter = Create(overrideBeatSaverService: beatSaverMock.Object,
-            overrideSongService: songServiceMock.Object);
-        await fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
+        await _fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
 
         // Make sure that the song was imported and downloaded
-        songServiceMock.Verify(s =>
+        _songServiceMock.Verify(s =>
                 s.ImportSongAsync(It.IsNotNull<ZipArchive>(), It.IsNotNull<string>()),
             Times.Once);
-        beatSaverMock.Verify(b => b.DownloadSongByHash(songHash), Times.Once);
+        _beatSaverServiceMock.Verify(b => b.DownloadSongByHash(songHash), Times.Once);
     }
 
     [Fact]
@@ -321,27 +297,18 @@ public class FileImporterTests
             new BPSong("", null, null)
         );
 
-        var beatSaverMock = new Mock<IBeatSaverService>();
-        beatSaverMock.Setup(b => b.DownloadSongByHash(It.IsAny<string>()))
+        _beatSaverServiceMock.Setup(b => b.DownloadSongByHash(It.IsAny<string>()))
             .ThrowsAsync(new HttpRequestException());
 
-        var songServiceMock = new Mock<ISongService>();
-        songServiceMock.Setup(s => s.GetSongsAsync())
-            .ReturnsAsync(new ConcurrentDictionary<string, Song>());
-
-        var playlistServiceMock = new Mock<IPlaylistService>();
-        playlistServiceMock.Setup(p => p.AddPlaylistAsync(It.IsAny<Playlist>()))
+        _playlistServiceMock.Setup(p => p.AddPlaylistAsync(It.IsAny<Playlist>()))
             .ReturnsAsync(expectedPlaylistId);
         
-        var fileImporter = Create(overrideBeatSaverService: beatSaverMock.Object,
-            overrideSongService: songServiceMock.Object,
-            overridePlaylistService: playlistServiceMock.Object);
         using var playlistStream = CreatePlaylistStream(examplePlaylist);
-        var result = await fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
+        var result = await _fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
         
         // The song download should have been attempted, but this should not have failed the playlist import
         Assert.Equal(expectedPlaylistId, result.ImportedPlaylistId);
-        beatSaverMock.Verify(b => b.DownloadSongByHash(It.IsAny<string>()), Times.Once);
+        _beatSaverServiceMock.Verify(b => b.DownloadSongByHash(It.IsAny<string>()), Times.Once);
     }
 
     [Fact]
@@ -353,33 +320,25 @@ public class FileImporterTests
         examplePlaylist.Songs = ImmutableList.Create(
             new BPSong("", null, mapKey)
         );
-        
-        var songServiceMock = new Mock<ISongService>();
-        songServiceMock.Setup(s => s.GetSongsAsync())
-            .ReturnsAsync(new ConcurrentDictionary<string, Song>());
-        
-        var beatSaverMock = new Mock<IBeatSaverService>();
-        beatSaverMock.Setup(b => b.DownloadSongByKey(It.IsAny<string>()))
+
+        _beatSaverServiceMock.Setup(b => b.DownloadSongByKey(It.IsAny<string>()))
             .ReturnsAsync(CreateEmptyZipStream());
         
-        var fileImporter = Create(overrideBeatSaverService: beatSaverMock.Object, overrideSongService: songServiceMock.Object);
         using var playlistStream = CreatePlaylistStream(examplePlaylist);
-        await fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
+        await _fileImporter.TryImportAsync(playlistStream, "my-playlist.json");
         
         // Verify that the song was downloaded by key, and NOT by hash
-        beatSaverMock.Verify(b => b.DownloadSongByKey(mapKey), Times.Once);
-        beatSaverMock.Verify(b => b.DownloadSongByHash(It.IsAny<string>()), Times.Never);
+        _beatSaverServiceMock.Verify(b => b.DownloadSongByKey(mapKey), Times.Once);
+        _beatSaverServiceMock.Verify(b => b.DownloadSongByHash(It.IsAny<string>()), Times.Never);
     }
 
     [Fact]
     public async Task ShouldFailWithUnknownExtension()
     {
-        var fileImporter = Create();
         using var blankContent = new MemoryStream();
         
-        var result = await fileImporter.TryImportAsync(blankContent, "my-file.unknown");
+        var result = await _fileImporter.TryImportAsync(blankContent, "my-file.unknown");
         // Since .unknown files are not supported, importing them should fail
         Assert.Equal(FileImportResultType.Failed, result.Type);
     }
-    
 }
