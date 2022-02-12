53d3b0649c42d1e068597d21ac45476ffea23c0d
Unicorn 12 <unicorn12@bmbf.dev>
2022-02-12 06:27:22 +0000

    Commit InvalidStageException.cs

---
+++ BMBF.Backend/InvalidStageException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BMBF.Backend;
+
+/// <summary>
+/// Thrown when attempting to run a setup stage when at the incorrect setup stage.
+/// </summary>
+public class InvalidStageException : Exception
+{
+    public InvalidStageException(string? message) : base(message) {}
+}
