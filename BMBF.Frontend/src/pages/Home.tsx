4cc66fa1cb4a96d251a804c4b9300f09da29249c
D3SOX <nico@d3sox.me>
2022-08-16 20:34:38 +0200

    feat: add random acronym to home page

--- BMBF.Frontend/src/pages/Home.tsx
+++ BMBF.Frontend/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { Stack, Title, Text } from '@mantine/core';
+import { generateAcronym } from '../utils';
 
 function Home() {
+  const acronym = generateAcronym();
+
   return (
     <Stack align="center">
       <img src="/logo.png" alt="Logo" />
       <Title>Home</Title>
-      <Text>Welcome to BMBF!</Text>
+      <Text>Welcome to BMBF! ({acronym})</Text>
     </Stack>
   );
 }
