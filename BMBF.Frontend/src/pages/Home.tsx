c6686d44c819fc8fc397800426af274d036ea6c5
D3SOX <nico@d3sox.me>
2022-08-16 16:26:10 +0200

    feat: add small home boilerplate

--- BMBF.Frontend/src/pages/Home.tsx
+++ BMBF.Frontend/src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { Stack, Title } from '@mantine/core';
+import { Stack, Title, Text } from '@mantine/core';
 
 function Home() {
   return (
     <Stack align="center">
       <img src="/logo.png" alt="Logo" />
       <Title>Home</Title>
+      <Text>Welcome to BMBF!</Text>
     </Stack>
   );
 }
