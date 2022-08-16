a7c887a2f3df24d9a7617456c2f10e6c8e6d31ef
D3SOX <nico@d3sox.me>
2022-08-16 21:34:52 +0200

    feat: navigation button extends button props

--- BMBF.Frontend/src/components/shell/NavigationButton.tsx
+++ BMBF.Frontend/src/components/shell/NavigationButton.tsx
@@ -1,14 +1,14 @@
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
-import { Button, useMantineTheme } from '@mantine/core';
+import { Button, ButtonProps, useMantineTheme } from '@mantine/core';
 import React from 'react';
 import { Page } from './AppHeader';
 import { useMediaQuery } from '@mantine/hooks';
 
-interface NavigationButtonProps {
+interface NavigationButtonProps extends ButtonProps {
   page: Page;
 }
 
-function NavigationButton({ page }: NavigationButtonProps) {
+function NavigationButton({ page, ...props }: NavigationButtonProps) {
   const resolved = useResolvedPath(page.to);
   const match = useMatch({ path: resolved.pathname, end: true });
 
@@ -16,15 +16,16 @@ function NavigationButton({ page }: NavigationButtonProps) {
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`, false);
 
   return (
-    <Button
-      component={Link}
-      to={page.to}
-      leftIcon={mobile ? undefined : page.icon}
-      variant={match ? 'filled' : 'default'}
-      radius="md"
-    >
-      {mobile ? page.icon : page.name}
-    </Button>
+    <Link to={page.to}>
+      <Button
+        leftIcon={mobile ? undefined : page.icon}
+        variant={match ? 'filled' : 'default'}
+        radius="md"
+        {...props}
+      >
+        {mobile ? page.icon : page.name}
+      </Button>
+    </Link>
   );
 }
 
