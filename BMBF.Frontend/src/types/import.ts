cd05cd95b664197b0cb03c5a7728720b9359e2d7
D3SOX <nico@d3sox.me>
2022-08-17 23:22:25 +0200

    feat: add missing import response types

--- BMBF.Frontend/src/types/import.ts
+++ BMBF.Frontend/src/types/import.ts
@@ -14,19 +14,9 @@ export interface BaseImportResponse {
   type: ImportType;
 }
 
-export interface FailedImportResponse extends BaseImportResponse {
-  type: ImportType.Failed;
-  error: string;
-}
-
 export interface SongImportResponse extends BaseImportResponse {
   type: ImportType.Song;
   importedSong: Song;
-}
-
-export interface PlaylistImportResponse extends BaseImportResponse {
-  type: ImportType.Playlist;
-  importedPlaylistId: string;
 }
 
 export interface ModImportResponse extends BaseImportResponse {
@@ -34,8 +24,30 @@ export interface ModImportResponse extends BaseImportResponse {
   importedMod: Mod;
 }
 
+export interface FileCopyImportResponse extends BaseImportResponse {
+  type: ImportType.FileCopy;
+  fileCopy: null;
+}
+
+export interface ConfigImportResponse extends BaseImportResponse {
+  type: ImportType.Config;
+  configModId: string;
+}
+
+export interface PlaylistImportResponse extends BaseImportResponse {
+  type: ImportType.Playlist;
+  importedPlaylistId: string;
+}
+
+export interface FailedImportResponse extends BaseImportResponse {
+  type: ImportType.Failed;
+  error: string;
+}
+
 export type ImportResponse =
-  | FailedImportResponse
   | SongImportResponse
+  | ModImportResponse
+  | FileCopyImportResponse
+  | ConfigImportResponse
   | PlaylistImportResponse
-  | ModImportResponse;
+  | FailedImportResponse;
