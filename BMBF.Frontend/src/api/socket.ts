a0b9629eb01e8842d680bf2f9a39d13a4fe085cc
D3SOX <nico@d3sox.me>
2022-09-07 13:08:13 +0200

    chore: disable `@typescript-eslint/no-explicit-any` for `socket.ts`

--- BMBF.Frontend/src/api/socket.ts
+++ BMBF.Frontend/src/api/socket.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { API_HOST, sendErrorNotification, sendSuccessNotification } from './base';
 import { MessageType, SocketMessage } from '../types/socket';
 import { playlistsStore } from './playlists';
