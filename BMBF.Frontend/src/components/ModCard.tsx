2ab1c4a7496aa134db67ae1201b55b9a0a3f11df
D3SOX <nico@d3sox.me>
2022-09-07 14:51:35 +0200

    feat: implement basic mod details

--- BMBF.Frontend/src/components/ModCard.tsx
+++ BMBF.Frontend/src/components/ModCard.tsx
@@ -1,14 +1,18 @@
 import { Mod } from '../types/mod';
-import { Card, Group, Image, Stack, Text, Button, Switch } from '@mantine/core';
-import { IconTrash } from '@tabler/icons';
+import { Card, Group, Image, Stack, Text, Button, Switch, Modal, Title } from '@mantine/core';
+import { IconEye, IconTrash } from '@tabler/icons';
 import { installMod, uninstallMod, unloadMod } from '../api/mods';
 import { API_ROOT } from '../api/base';
+import { useMemo, useState } from 'react';
 
 interface ModCardProps {
   mod: Mod;
 }
 
 function SongCard({ mod }: ModCardProps) {
+  const [showDetails, setShowDetails] = useState(false);
+  const dependencies = useMemo(() => Object.entries(mod.dependencies), [mod.dependencies]);
+
   return (
     <Card title={mod.id}>
       <Group align="start" noWrap>
@@ -25,6 +29,36 @@ function SongCard({ mod }: ModCardProps) {
                 event.currentTarget.checked ? installMod(mod) : uninstallMod(mod);
               }}
             />
+            <Button leftIcon={<IconEye />} variant="light" onClick={() => setShowDetails(true)}>
+              Details
+            </Button>
+            <Modal
+              opened={showDetails}
+              onClose={() => setShowDetails(false)}
+              title={<Title order={3}>Details for {mod.name}</Title>}
+              centered
+            >
+              <Stack spacing={5}>
+                <Stack spacing={1}>
+                  <Title order={4}>Description</Title>
+                  <Text>{mod.description}</Text>
+                </Stack>
+                <Stack spacing={1}>
+                  <Title order={4}>Dependencies</Title>
+                  {dependencies.length ? (
+                    <div>
+                      {dependencies.map(([dependencyName, dependencyVersion]) => (
+                        <Text key={dependencyName}>
+                          {dependencyName} {dependencyVersion}
+                        </Text>
+                      ))}
+                    </div>
+                  ) : (
+                    <Text>None</Text>
+                  )}
+                </Stack>
+              </Stack>
+            </Modal>
             <Button leftIcon={<IconTrash />} color="red" variant="light" onClick={() => unloadMod(mod)}>
               Remove
             </Button>
