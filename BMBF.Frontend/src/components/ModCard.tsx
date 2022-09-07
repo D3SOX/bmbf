572706eb906e778da471b33ef0462d9fda8d0dbe
D3SOX <nico@d3sox.me>
2022-09-07 15:14:49 +0200

    feat: mod remove confirmation modal

--- BMBF.Frontend/src/components/ModCard.tsx
+++ BMBF.Frontend/src/components/ModCard.tsx
@@ -1,20 +1,103 @@
 import { Mod } from '../types/mod';
-import { Badge, Card, Group, Image, Stack, Text, Button, Switch, Modal, Title } from '@mantine/core';
-import { IconEye, IconTrash } from '@tabler/icons';
-import { installMod, uninstallMod, unloadMod } from '../api/mods';
+import {
+  Badge,
+  Card,
+  Group,
+  Image,
+  Stack,
+  Text,
+  Button,
+  Switch,
+  Modal,
+  Title,
+  List,
+} from '@mantine/core';
+import { IconArrowBackUp, IconEye, IconTrash } from '@tabler/icons';
+import { installMod, modsStore, uninstallMod, unloadMod } from '../api/mods';
 import { API_ROOT } from '../api/base';
 import { useMemo, useState } from 'react';
+import { useSnapshot } from 'valtio';
 
 interface ModCardProps {
   mod: Mod;
 }
 
 function SongCard({ mod }: ModCardProps) {
-  const [showDetails, setShowDetails] = useState(false);
+  const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [showDetailsModal, setShowDetailsModal] = useState(false);
   const dependencies = useMemo(() => Object.entries(mod.dependencies), [mod.dependencies]);
+
+  const { mods } = useSnapshot(modsStore);
+  const dependants = useMemo(() => {
+    // installed mods that depend on this mod
+    return mods.filter(
+      otherMod => otherMod.id !== mod.id && otherMod.dependencies[mod.id] !== undefined
+    );
+  }, [mod, mods]);
 
   return (
     <Card title={mod.id}>
+      <Modal
+        opened={showDetailsModal}
+        onClose={() => setShowDetailsModal(false)}
+        title={<Title order={3}>Details for {mod.name}</Title>}
+        centered
+      >
+        <Stack spacing={5}>
+          <Stack spacing={1}>
+            <Title order={4}>Description</Title>
+            <Text>{mod.description}</Text>
+          </Stack>
+          <Stack spacing={1}>
+            <Title order={4}>Dependencies</Title>
+            {dependencies.length ? (
+              <div>
+                {dependencies.map(([dependencyName, dependencyVersion]) => (
+                  <Text key={dependencyName}>
+                    {dependencyName} {dependencyVersion}
+                  </Text>
+                ))}
+              </div>
+            ) : (
+              <Text>None</Text>
+            )}
+          </Stack>
+        </Stack>
+      </Modal>
+
+      <Modal
+        opened={showRemoveModal}
+        onClose={() => setShowRemoveModal(false)}
+        title={<Title order={3}>Remove {mod.name}?</Title>}
+        withCloseButton={false}
+        centered
+      >
+        <Stack>
+          {dependants.length ? (
+            <Stack spacing={1}>
+              <Title order={4}>Warning</Title>
+              <Text>This mod is required by the following mods:</Text>
+              <List>
+                {dependants.map(m => (
+                  <List.Item key={m.id}>{m.name}</List.Item>
+                ))}
+              </List>
+              <Text>Removing this mod will also remove the above mods.</Text>
+            </Stack>
+          ) : (
+            <Text>There are no mods installed that depend on this mod.</Text>
+          )}
+          <Group grow>
+            <Button leftIcon={<IconArrowBackUp />} onClick={() => setShowRemoveModal(false)}>
+              No, don't remove
+            </Button>
+            <Button leftIcon={<IconTrash />} color="red" onClick={() => unloadMod(mod)}>
+              Yes, remove
+            </Button>
+          </Group>
+        </Stack>
+      </Modal>
+
       <Group align="start" noWrap>
         <Image src={`${API_ROOT}/mods/cover/${mod.id}`} alt="Cover" width={150} radius="md" />
         <Stack>
@@ -31,37 +114,15 @@ function SongCard({ mod }: ModCardProps) {
                 event.currentTarget.checked ? installMod(mod) : uninstallMod(mod);
               }}
             />
-            <Button leftIcon={<IconEye />} variant="light" onClick={() => setShowDetails(true)}>
+            <Button leftIcon={<IconEye />} variant="light" onClick={() => setShowDetailsModal(true)}>
               Details
             </Button>
-            <Modal
-              opened={showDetails}
-              onClose={() => setShowDetails(false)}
-              title={<Title order={3}>Details for {mod.name}</Title>}
-              centered
+            <Button
+              leftIcon={<IconTrash />}
+              color="red"
+              variant="light"
+              onClick={() => setShowRemoveModal(true)}
             >
-              <Stack spacing={5}>
-                <Stack spacing={1}>
-                  <Title order={4}>Description</Title>
-                  <Text>{mod.description}</Text>
-                </Stack>
-                <Stack spacing={1}>
-                  <Title order={4}>Dependencies</Title>
-                  {dependencies.length ? (
-                    <div>
-                      {dependencies.map(([dependencyName, dependencyVersion]) => (
-                        <Text key={dependencyName}>
-                          {dependencyName} {dependencyVersion}
-                        </Text>
-                      ))}
-                    </div>
-                  ) : (
-                    <Text>None</Text>
-                  )}
-                </Stack>
-              </Stack>
-            </Modal>
-            <Button leftIcon={<IconTrash />} color="red" variant="light" onClick={() => unloadMod(mod)}>
               Remove
             </Button>
           </Group>
