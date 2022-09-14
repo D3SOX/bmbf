8ab146f2fedc1643bf214aa054ea8238a2a43a42
Unicorn 12 <unicorn12@bmbf.dev>
2022-09-14 21:47:53 +0100

    Allow code 400 for import requests

--- BMBF.Frontend/src/api/import.tsx
+++ BMBF.Frontend/src/api/import.tsx
@@ -2,12 +2,18 @@ import { backendRequest, sendErrorNotification } from './base';
 import { ImportResponse, ImportType } from '../types/import';
 
 export async function importViaUrl(url: string): Promise<void> {
-  const data = await backendRequest('import/url', {
-    method: 'POST',
-    body: `"${url}"`,
-  });
-  if (data.ok) {
+  const data = await backendRequest(
+    'import/url',
+    {
+      method: 'POST',
+      body: `"${url}"`,
+    },
+    [400]
+  );
+
+  if (data.ok || data.status == 400) {
     const json: ImportResponse = await data.json();
+
     if (json.type === ImportType.Failed) {
       sendErrorNotification(json.error);
     }
@@ -16,15 +22,21 @@ export async function importViaUrl(url: string): Promise<void> {
 
 export async function importViaFile(file: File): Promise<void> {
   if (file.name.endsWith('.zip') || file.name.endsWith('.qmod') || file.name.endsWith('.bplist')) {
-    const data = await backendRequest('import/file', {
-      method: 'POST',
-      body: file,
-      headers: {
-        'Content-Disposition': `attachment; filename="${file.name}"`,
+    const data = await backendRequest(
+      'import/file',
+      {
+        method: 'POST',
+        body: file,
+        headers: {
+          'Content-Disposition': `attachment; filename="${file.name}"`,
+        },
       },
-    });
-    if (data.ok) {
+      [400]
+    );
+
+    if (data.ok || data.status == 400) {
       const json: ImportResponse = await data.json();
+
       if (json.type === ImportType.Failed) {
         sendErrorNotification(json.error);
       }
